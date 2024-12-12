d59f966f9c65fa4de895d8a73801dbffc99a49f1
Guy Stephane <gstephane138@gmail.com>
2024-12-13 00:33:38 +0100

    fixed dynamically render time table for each department

--- src/app/timetables/[departmentsID]/[levelID]/[semester]/page.jsx
+++ src/app/timetables/[departmentsID]/[levelID]/[semester]/page.jsx
@@ -4,11 +4,28 @@ import { useRouter } from "next/navigation";
 import { MdPerson } from "react-icons/md";
 import { MdCases } from "react-icons/md";
 import styles from "./page.module.css";
-import { arr, handleTableLogic } from "@/app/scripts/timetable-logic";
+import getTeacherAvailability from "../../../../../../lib/getTeacherAvailability";
 
 const semesters = ({ params }) => {
   const { departmentsID, semester, levelID } = React.use(params);
+  //fetching the teacher availabilities
+  const [availabilities, setAvailabilities] = React.useState([{}]);
 
+  React.useEffect(() => {
+    const getAvailabilities = async () => {
+      try {
+        const response = await getTeacherAvailability();
+        if (response.error) {
+          console.log(response.error);
+        } else {
+          setAvailabilities(response);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getAvailabilities();
+  }, []);
   //rerouting to the 404 page if the semester entered on the URL is not found.
   const router = useRouter();
   if (
@@ -52,20 +69,41 @@ const semesters = ({ params }) => {
     }
   }, [semester, departmentsID]);
 
-  //fetching lecturers availability with useEffect using the tableLogic function, which keeps the data in the arr variable being destructured alongside the tableLogic function on line 7
-  React.useEffect(() => {
-    const fetchAvailability = async () => {
-      try {
-        await handleTableLogic();
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    fetchAvailability();
-  }, []);
-  const convertToNumber = (data) => {
-    
-  }
+  //convert each semester from the dynamic route from words to number.
+  const [number, setNumber] = React.useState(null);
+  const convertSemester = (data) => {
+    if (data === "semester-one") {
+      setNumber(1);
+    } else if (data === "semester-two") {
+      setNumber(2);
+    } else if (data === "semester-three") {
+      setNumber(3);
+    } else if (data === "semester-four") {
+      setNumber(4);
+    } else if (data === "degree-one") {
+      setNumber("Degree 1");
+    } else {
+      setNumber("Degree 2");
+    }
+  };
+  React.useState(() => {
+    convertSemester(semester);
+  }, [semester]);
+
+  //convert each level to the suitable form as in the database
+  const [level, setLevel] = React.useState(null);
+  const convertLevel = (level) => {
+    if (level === "level-one") {
+      setLevel("Level One");
+    } else if (level === "level-two") {
+      setLevel("Level Two");
+    } else {
+      setLevel("Degree");
+    }
+  };
+  React.useState(() => {
+    convertLevel(levelID);
+  }, [levelID]);
   return (
     <>
       <div className={styles.container}>
@@ -92,13 +130,14 @@ const semesters = ({ params }) => {
                 <p>
                   {" "}
                   <MdCases size={20} /> &nbsp; &nbsp;{" "}
-                  {arr
+                  {availabilities
                     .filter(
                       (teacher) =>
-                        teacher.department === "SWE" &&
+                        teacher.department === departmentsID.toUpperCase() &&
                         teacher.time === "8:00 - 10:00" &&
                         teacher.day === "Monday" &&
-                        teacher.level === "Level One"
+                        teacher.level === level &&
+                        teacher.semester === number
                     )
                     .map((teacher) => (
                       <span key={teacher.id}> {teacher.course}</span>
@@ -110,13 +149,14 @@ const semesters = ({ params }) => {
                 <p className="lecturer-info">
                   <MdPerson size={30} />
                   (M):{" "}
-                  {arr
+                  {availabilities
                     .filter(
                       (teacher) =>
-                        teacher.department === "SWE" &&
+                        teacher.department === departmentsID.toUpperCase() &&
                         teacher.time === "8:00 - 10:00" &&
                         teacher.day === "Monday" &&
-                        teacher.level === "Level One"
+                        teacher.level === level &&
+                        teacher.semester === number
                     )
                     .map((teacher) => (
                       <span key={teacher.id}> {teacher.names}</span>
@@ -125,273 +165,18 @@ const semesters = ({ params }) => {
                 <p>
                   <MdPerson size={30} />
                   (B):{" "}
-                  {arr
+                  {availabilities
                     .filter(
                       (teacher) =>
-                        teacher.department === "SWE" &&
+                        teacher.department === departmentsID.toUpperCase() &&
                         teacher.time === "8:00 - 10:00" &&
                         teacher.day === "Monday" &&
-                        teacher.level === "Level One"
+                        teacher.level === level &&
+                        teacher.semester === number
                     )
                     .map((teacher) => (
-                      <span key={teacher.id}> {teacher.backupTeacher}</span>
+                      <span key={teacher.id}> {teacher.backupTeacherNames}</span>
                     ))}
-                </p>
-              </td>
-              <td>
-                <p>8:00 - 10:00</p>
-                <p>
-                  {" "}
-                  <MdCases size={20} /> &nbsp; &nbsp;{" "}
-                </p>
-                <p>
-                  <i> {departmentsID.toUpperCase()}</i>
-                </p>
-                <p className="lecturer-info">
-                  <MdPerson size={30} />
-                  (M):{" "}
-                </p>
-                <p>
-                  <MdPerson size={30} />
-                  (B):{" "}
-                </p>
-              </td>
-              <td>
-                <p>8:00 - 10:00</p>
-                <p>
-                  {" "}
-                  <MdCases size={20} /> &nbsp; &nbsp;{" "}
-                </p>
-                <p>
-                  <i> {departmentsID.toUpperCase()}</i>
-                </p>
-                <p className="lecturer-info">
-                  <MdPerson size={30} />
-                  (M):{" "}
-                </p>
-                <p>
-                  <MdPerson size={30} />
-                  (B):{" "}
-                </p>
-              </td>
-              <td>
-                <p>8:00 - 10:00</p>
-                <p>
-                  {" "}
-                  <MdCases size={20} /> &nbsp; &nbsp;{" "}
-                </p>
-                <p>
-                  <i> {departmentsID.toUpperCase()}</i>
-                </p>
-                <p className="lecturer-info">
-                  <MdPerson size={30} />
-                  (M):{" "}
-                </p>
-                <p>
-                  <MdPerson size={30} />
-                  (B):{" "}
-                </p>
-              </td>
-              <td>
-                <p>8:00 - 10:00</p>
-                <p>
-                  {" "}
-                  <MdCases size={20} /> &nbsp; &nbsp;{" "}
-                </p>
-                <p>
-                  <i> {departmentsID.toUpperCase()}</i>
-                </p>
-                <p className="lecturer-info">
-                  <MdPerson size={30} />
-                  (M):{" "}
-                </p>
-                <p>
-                  <MdPerson size={30} />
-                  (B):{" "}
-                </p>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <p>8:00 - 10:00</p>
-                <p>
-                  {" "}
-                  <MdCases size={20} /> &nbsp; &nbsp;{" "}
-                </p>
-                <p>
-                  <i> {departmentsID.toUpperCase()}</i>
-                </p>
-                <p className="lecturer-info">
-                  <MdPerson size={30} />
-                  (M):{" "}
-                </p>
-                <p>
-                  <MdPerson size={30} />
-                  (B):{" "}
-                </p>
-              </td>
-              <td>
-                <p>8:00 - 10:00</p>
-                <p>
-                  {" "}
-                  <MdCases size={20} /> &nbsp; &nbsp;{" "}
-                </p>
-                <p>
-                  <i> {departmentsID.toUpperCase()}</i>
-                </p>
-                <p className="lecturer-info">
-                  <MdPerson size={30} />
-                  (M):{" "}
-                </p>
-                <p>
-                  <MdPerson size={30} />
-                  (B):{" "}
-                </p>
-              </td>
-              <td>
-                <p>8:00 - 10:00</p>
-                <p>
-                  {" "}
-                  <MdCases size={20} /> &nbsp; &nbsp;{" "}
-                </p>
-                <p>
-                  <i> {departmentsID.toUpperCase()}</i>
-                </p>
-                <p className="lecturer-info">
-                  <MdPerson size={30} />
-                  (M):{" "}
-                </p>
-                <p>
-                  <MdPerson size={30} />
-                  (B):{" "}
-                </p>
-              </td>
-              <td>
-                <p>8:00 - 10:00</p>
-                <p>
-                  {" "}
-                  <MdCases size={20} /> &nbsp; &nbsp;{" "}
-                </p>
-                <p>
-                  <i> {departmentsID.toUpperCase()}</i>
-                </p>
-                <p className="lecturer-info">
-                  <MdPerson size={30} />
-                  (M):{" "}
-                </p>
-                <p>
-                  <MdPerson size={30} />
-                  (B):{" "}
-                </p>
-              </td>
-              <td>
-                <p>8:00 - 10:00</p>
-                <p>
-                  {" "}
-                  <MdCases size={20} /> &nbsp; &nbsp;{" "}
-                </p>
-                <p>
-                  <i> {departmentsID.toUpperCase()}</i>
-                </p>
-                <p className="lecturer-info">
-                  <MdPerson size={30} />
-                  (M):{" "}
-                </p>
-                <p>
-                  <MdPerson size={30} />
-                  (B):{" "}
-                </p>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <p>8:00 - 10:00</p>
-                <p>
-                  {" "}
-                  <MdCases size={20} /> &nbsp; &nbsp;{" "}
-                </p>
-                <p>
-                  <i> {departmentsID.toUpperCase()}</i>
-                </p>
-                <p className="lecturer-info">
-                  <MdPerson size={30} />
-                  (M):{" "}
-                </p>
-                <p>
-                  <MdPerson size={30} />
-                  (B):{" "}
-                </p>
-              </td>
-              <td>
-                <p>8:00 - 10:00</p>
-                <p>
-                  {" "}
-                  <MdCases size={20} /> &nbsp; &nbsp;{" "}
-                </p>
-                <p>
-                  <i> {departmentsID.toUpperCase()}</i>
-                </p>
-                <p className="lecturer-info">
-                  <MdPerson size={30} />
-                  (M):{" "}
-                </p>
-                <p>
-                  <MdPerson size={30} />
-                  (B):{" "}
-                </p>
-              </td>
-              <td>
-                <p>8:00 - 10:00</p>
-                <p>
-                  {" "}
-                  <MdCases size={20} /> &nbsp; &nbsp;{" "}
-                </p>
-                <p>
-                  <i> {departmentsID.toUpperCase()}</i>
-                </p>
-                <p className="lecturer-info">
-                  <MdPerson size={30} />
-                  (M):{" "}
-                </p>
-                <p>
-                  <MdPerson size={30} />
-                  (B):{" "}
-                </p>
-              </td>
-              <td>
-                <p>8:00 - 10:00</p>
-                <p>
-                  {" "}
-                  <MdCases size={20} /> &nbsp; &nbsp;{" "}
-                </p>
-                <p>
-                  <i> {departmentsID.toUpperCase()}</i>
-                </p>
-                <p className="lecturer-info">
-                  <MdPerson size={30} />
-                  (M):{" "}
-                </p>
-                <p>
-                  <MdPerson size={30} />
-                  (B):{" "}
-                </p>
-              </td>
-              <td>
-                <p>8:00 - 10:00</p>
-                <p>
-                  {" "}
-                  <MdCases size={20} /> &nbsp; &nbsp;{" "}
-                </p>
-                <p>
-                  <i> {departmentsID.toUpperCase()}</i>
-                </p>
-                <p className="lecturer-info">
-                  <MdPerson size={30} />
-                  (M):{" "}
-                </p>
-                <p>
-                  <MdPerson size={30} />
-                  (B):{" "}
                 </p>
               </td>
             </tr>
